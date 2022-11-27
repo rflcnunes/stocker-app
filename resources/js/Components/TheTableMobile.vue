52b430e1bd75364d4618058266c46a5b2e3466f2
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-11-27 01:49:18 -0300

    added table mobile component

---
+++ resources/js/Components/TheTableMobile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="product in products" :key="product.id">
+        <TheNotification v-bind:title="product.name" v-bind:description="formatter(product)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNotification from "@/Components/TheNotification.vue";
+
+export default {
+  name: "TheTableMobile",
+  components: {
+    TheNotification,
+  },
+  data() {
+    return {
+      //
+    };
+  },
+  props: {
+    products: Array,
+  },
+  methods: {
+    formatter: function (value) {
+      console.log(value);
+      if (value.sku) {
+        return "Your product SKU is " + value.sku + "." + " Your product quantity is " + value.quantity + ".";
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -2,32 +2,59 @@
   <Head title="Products" />
   <AuthenticatedLayout>
     <template #header>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Products</h2>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Products
+      </h2>
     </template>
-    <div>{{ products }}</div>
-
-    <!-- <div v-for="user in users" :key="user.id">
-      <Link :href="`/users/${user.id}`">
-        {{ user.name }}
-      </Link>
-      <div>{{ user.email }}</div>
-    </div> -->
+    <template #action>
+      <button id="create-product-button-header">Create Product</button>
+    </template>
+    <TheTable v-if="!mobile" v-bind:products="products" />
+    <TheTableMobile v-if="mobile" v-bind:products="products" />
   </AuthenticatedLayout>
 </template>
 
 <script>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import InputError from "@/Components/InputError.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import { useForm, Head } from "@inertiajs/inertia-vue3";
+import { Head } from "@inertiajs/inertia-vue3";
+import TheTable from "@/Components/TheTable.vue";
+import TheTableMobile from "@/Components/TheTableMobile.vue";
 
 export default {
   components: {
     AuthenticatedLayout,
+    TheTable,
+    TheTableMobile,
     Head,
+  },
+  data() {
+    return {
+      editing: false,
+      mobile: false,
+      tablet: false,
+    };
   },
   props: {
     products: Array,
   },
+  created() {
+    this.mobile = window.innerWidth < 640;
+  },
+  methods: {
+    //
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+#create-product-button-header {
+  border: 1px solid #e2e8f0;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #e2e8f0;
+
+  &:hover {
+    background-color: #cbd5e0;
+  }
+}
+</style>

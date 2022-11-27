aec3c563771b1b056a0c325898a3993a7cb926f4
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-11-27 17:24:25 -0300

    added route create for component in web

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/products', [ProductController::class, 'index'])->name('products');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

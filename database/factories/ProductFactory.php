f46d75c9b2cd6f9538c95996f86a181652cad0a3
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-11-27 01:47:47 -0300

    adjustments in product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,9 +16,31 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $nameProduct = [
+            'Nintendo Switch OLED',
+            'PlayStation 5',
+            'Xbox Series X',
+            'Xbox Series S',
+            'PlayStation 4',
+            'Xbox One',
+            'Nintendo Switch',
+            'Nintendo 3DS',
+            'Nintendo DS',
+            'Nintendo Wii',
+            'Nintendo GameCube',
+            'Nintendo 64',
+            'Nintendo Entertainment System',
+            'JBL Quantumm',
+            'JBL Quantum ONE',
+            'Macbook Pro',
+            'Macbook Air',
+            'Macbook Air M1',
+            'Macbook Pro M1',
+        ];
+
         return [
             'sku' => $this->faker->unique()->ean8,
-            'name' => $this->faker->unique()->words(3, true),
+            'name' => $this->faker->randomElement($nameProduct),
             'description' => $this->faker->optional()->paragraph,
             'quantity' => $this->faker->numberBetween(0, 100),
         ];

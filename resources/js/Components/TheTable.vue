caf074aad86c802d1e1eb9d28ba05491c1204797
Rafael <rafael@Rafaels-MacBook-Air.local>
2022-12-04 23:14:26 -0300

    added dark mode and adjustments in table

--- resources/js/Components/TheTable.vue
+++ resources/js/Components/TheTable.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="product-table">
+  <div class="product-table" :class="theme">
     <table>
       <tr>
+        <th>
+          <input type="checkbox">
+        </th>
+        <th>SKU</th>
         <th>Name</th>
+        <th>Price</th>
+        <th>Category</th>
         <th>Description</th>
         <th>Quantity</th>
         <th></th>
       </tr>
       <tr v-for="product in products" :key="product.id">
+        <td>
+          <input type="checkbox">
+        </td>
+        <td class="font-semibold">{{ product.sku }}</td>
         <td class="font-semibold">{{ product.name }}</td>
+        <td class="font-semibold">{{ product.price }}</td>
+        <td class="font-semibold">
+          <div id="circle"></div>
+          {{ product.category }}
+        </td>
         <td class="url text-slate-600">{{ product.description || "-" }}</td>
         <td class="text-slate-600">{{ product.quantity }}</td>
         <td>
@@ -55,11 +70,23 @@ export default {
         message: '',
         show: false,
         duration: 3000,
-      }
+      },
+      darkMode: false,
     };
   },
   props: {
     products: Array,
+  },
+  created() {
+    this.darkMode = (localStorage.getItem("theme") === "dark");
+  },
+  updated() {
+    this.darkMode = (localStorage.getItem('theme') === 'dark');
+  },
+  computed: {
+    theme() {
+      return (this.darkMode) ? 'product-table-dark' : 'product-table-light';
+    },
   },
   methods: {
     editProduct(id) {
@@ -67,8 +94,6 @@ export default {
     },
     deleteProduct(id, name) {
       console.log(id);
-      // this.$inertia.delete(`/api/product/${id}`);
-
       axios.delete(`/api/product/${id}`)
         .then(response => {
           console.log(response);
@@ -78,46 +103,85 @@ export default {
         .catch(error => {
           console.log(error);
         });
-
-
     },
     openToast(message) {
       this.toast.show = true;
       this.toast.message = message;
+    },
+    consoleNoTheme() {
+      console.log(localStorage.getItem('theme'));
     }
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../css/_variables.scss";
+
+#circle {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+.product-table-dark {
+  border: 1px solid $border-dark-color;
+  background-color: $color-secondary-dark;
+
+  table {
+    th {
+      color: $color-tertiary-light;
+      border-bottom: 1px solid $color-tertiary-dark;
+      padding: 0.5rem;
+    }
+
+    td {
+      color: $color-tertiary-light;
+      border-bottom: 1px solid $color-tertiary-dark;
+      padding: 0.5rem;
+    }
+
+    .url {
+      color: $color-secondary-light;
+    }
+  }
+}
+
+.product-table-light {
+  border: 1px solid $border-light-color;
+  background-color: $color-primary-light;
+
+  table {
+    color: $color-primary-dark;
+
+    th {
+      border-bottom: 1px solid $color-tertiary-light;
+      font-weight: $font-weight-bold;
+      padding: 0.5rem;
+    }
+
+    td {
+      border-bottom: 1px solid $color-tertiary-light;
+      padding: 0.5rem;
+    }
+  }
+}
+
 .product-table {
   width: 100%;
-  border: 1px solid rgba(114, 114, 114, 0.292);
   border-radius: 10px;
 
   table {
     border-collapse: collapse;
 
-    tr {
-      border-bottom: 1px solid rgba(114, 114, 114, 0.292);
-    }
-
     th {
       padding: 10px;
       text-align: left;
-      background-color: rgba(209, 209, 209, 0.103);
-
-      &:first-child {
-        border-top-left-radius: 10px;
-      }
-
-      &:last-child {
-        border-top-right-radius: 10px;
-      }
     }
 
     td {
-      padding: 10px;
       text-align: left;
 
       .url {
